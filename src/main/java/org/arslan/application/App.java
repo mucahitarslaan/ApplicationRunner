5dd51f423826b2c3ed433f242b4e1a38ef51a128
mucahitarslaan <mucahitarslaan@gmail.com>
2022-09-15 09:25:50 +0300

    Application Properties dosyasi ile Value Annotation baglantisi

--- src/main/java/org/arslan/application/App.java
+++ src/main/java/org/arslan/application/App.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GenericAppApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(GenericAppApplication.class, args);
 	}
 
